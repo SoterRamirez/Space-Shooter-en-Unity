8ccc2828ab3d8ced65f9c305b64e92a7a4d7ba97
SoterRamirez <soterrc@gmail.com>
2019-03-11 20:57:03 -0600

    06 Creando el disparo, se crea el disparo

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Mover : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rig;
+
+    void Awake()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig.velocity = transform.forward * speed;   
+    }
+}
